e97ae98a8d4f2f258dd038ed1f7411b55758c701
jesus-eo <jesusmanuel.espinar@iesdonana.org>
2021-10-14 21:16:41 +0200

    Ejercicio tres

---
+++ Unidad2/u2e3_comparacion.js
@@ -0,0 +1,20 @@
+let a = (10 == 10);
+let b = (10 === 10);
+let c = (10 === 10.0);
+let d = "Laura" == "laura";
+let e = "Laura" > "Laura";
+let f = "Laura" < "Laura";
+let g = "123" == 123;
+let h = "123" === 123;
+let i = parseInt("123") === 123;
+
+
+alert("La operacion 10 == 10 es: " + a);
+alert("La operacion 10 === 10 es: " + b);
+alert("La operacion 10 === 10.0 es: " + c);
+alert('El resultado de "Laura" == "laura" es: ' + d);
+alert('El resultado de "Laura" > "laura" es: ' + e);
+alert('El resultado de "Laura" < "laura" es: ' + f);
+alert('El resultado de "123" == 123 es: ' + g);
+alert('El resultado de "123" === 123 es: ' + h);
+alert('El resultado de parseInt("123") === 123 es: ' + i);
